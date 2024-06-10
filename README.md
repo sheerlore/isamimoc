ff76c894432ff28e6a366648b20190cbe93e8f43
sheerlore <forgivemyblue13@gmail.com>
2024-06-11 07:35:53 +0900

    click canvas example

--- README.md
+++ README.md
@@ -6,3 +6,15 @@ FastAPI + Vue3で遊ぶ用
 - Vue3 Pooling
 - Vue3 Composables
 - Vue3 Pinia State Management
+
+```bash
+$ source venv/bin/activate
+$ diactivate
+
+$ cd client
+$ npm run dev
+
+$ cd server
+$ fastapi dev main.py
+
+```
--- client/src/components/Canvas.vue
+++ client/src/components/Canvas.vue
@@ -1,42 +1,53 @@
 <script setup lang="ts">
 
-import {ref,  onMounted, watch} from 'vue'
+import {ref,  onMounted, watch, onUnmounted} from 'vue'
 const canvasRef = ref<HTMLCanvasElement | null>(null)
 const ctx = ref<CanvasRenderingContext2D | null>(null)
 
-const clear = () => {
-  if (ctx.value && canvasRef.value) {
+onMounted(() => {
+  if (canvasRef.value) {
+    canvasRef.value.width = window.innerWidth
+    canvasRef.value.height = window.innerHeight
+    ctx.value = canvasRef.value.getContext('2d')
+    canvasRef.value.addEventListener('mousedown', mouseDown);
+  }
+  loop()
+})
+
+onUnmounted(() => {
+  if (canvasRef.value) {
+    canvasRef.value.removeEventListener('mousedown', mouseDown)
+  }
+})
+
+const loop = () => {
+  if (canvasRef.value && ctx.value) {
     ctx.value.fillStyle = "#cccccc"
     ctx.value.fillRect(0, 0, canvasRef.value.width, canvasRef.value.height)
   }
 }
 
-onMounted(() => {
-  if (canvasRef.value) {
-    canvasRef.value.width = window.innerWidth
-    canvasRef.value.height = window.innerHeight - 300 
-    ctx.value = canvasRef.value.getContext('2d')
+const pos = (e: MouseEvent) => {
+  if (!canvasRef.value) return
+  return {
+    x: e.clientX - canvasRef.value?.getBoundingClientRect().left,
+    y: e.clientY - canvasRef.value?.getBoundingClientRect().top,
   }
-  clear()
-})
-watch(() => canvasRef.value, () => {
-  console.log("change canvas")
-}, {deep: true})
+}
+
+const mouseDown = (e: MouseEvent) => {
+  const p = pos(e);
+  if (canvasRef.value && ctx.value && p) {
+    ctx.value.fillStyle = "#ffffff"
+    ctx.value.fillRect(p.x, p.y, 15, 15);
+  }
+}
 </script>
 
 <template>
-  <!-- <p>w: {{ width }} h: {{ height }} </p> -->
-  <canvas
-    id="canvas"
-    ref="canvasRef"
-  ></canvas>
+  <canvas ref="canvasRef"></canvas>
 </template>
 
 <style scoped>
-#canvas {
-  width: 100%;
-  margin: 0;
-  padding: 0;
-}
 </style>
 
