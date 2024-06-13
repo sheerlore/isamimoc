16af2d5eeb680bc67a6d0e88560eb3d998ad6aa5
sheerlore <forgivemyblue13@gmail.com>
2024-06-14 04:26:07 +0900

    change dist pos

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: "../server/dist",
+    manifest: true,
+    emptyOutDir: false
+  }
 })
